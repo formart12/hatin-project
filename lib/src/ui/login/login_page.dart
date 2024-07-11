9c0444b07d733340b742ad5a27fdcde64b319094
formart12 <formart12@naver.com>
2024-07-11 14:06:08 +0900

    로그인 UI 완성

--- lib/src/ui/login/login_page.dart
+++ lib/src/ui/login/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:hatin/src/widget/gradient_button.dart';
 
 class LoginPage extends StatelessWidget {
@@ -28,9 +27,11 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             _text(),
-            _inputField("아이디", "아이디를 입력해주세요.", false),
+            const UserInfo(
+                label: "아이디", hintText: "아이디를 입력해주세요.", isPassword: false),
             _findid(),
-            _inputField("비밀번호", "비밀번호를 입력해주세요.", true),
+            const UserInfo(
+                label: "비밀번호", hintText: "비밀번호를 입력해주세요.", isPassword: true),
             _findpassword(),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 50),
@@ -39,7 +40,7 @@ class LoginPage extends StatelessWidget {
                 onPressed: () {},
               ),
             ),
-            _btnsignup(),
+            btnSignup(),
           ],
         ),
       ),
@@ -59,40 +60,6 @@ class LoginPage extends StatelessWidget {
           color: Color(0xff111111),
         ),
       ),
-    );
-  }
-
-  Widget _inputField(String label, String hintText, bool isPassword) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            label,
-            style: const TextStyle(fontSize: 16),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: const Color(0xffF1F3F5),
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: TextField(
-              obscureText: isPassword,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: hintText,
-                hintStyle: const TextStyle(
-                  color: Color(0xff9B9B9B),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 
@@ -146,7 +113,7 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _btnsignup() {
+  Widget btnSignup() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 50),
       child: Row(
@@ -165,6 +132,54 @@ class LoginPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final bool isPassword;
+
+  const UserInfo({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.isPassword,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            label,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffF1F3F5),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextField(
+              obscureText: isPassword,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: hintText,
+                hintStyle: const TextStyle(
+                  color: Color(0xff9B9B9B),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
